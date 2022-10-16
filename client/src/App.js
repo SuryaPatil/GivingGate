54f14439223194d345cbd11d93f79c083dc5d78a
Surya Patil <patil.surya01@gmail.com>
2022-10-15 22:38:25 -0400

    second commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,27 @@
 import { Fragment } from 'react';
 import './App.css';
+import {BrowserRouter as Router, 
+  Routes,
+  Navigate,
+  Link,
+  Route} from "react-router-dom";
+  import Apply from "./components/Apply";
+  import Corporations from "./components/Corporations";
+  import Contact from "./components/Contact";
+
 
 function App() {
-  return <Fragment></Fragment>
+  return (
+    <div className="container">
+      <Router>
+        <Routes>
+        <Route path="/Apply" element={<Apply/>}/>
+        <Route path="/Corporations" element={<Corporations/>}/>
+        <Route path="/Contact" element={<Contact/>}/>
+        </Routes>
+      </Router>
+    </div>
+  )
 }
 
 export default App;
---
+++ client/src/components/Corporations.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Corporations = () => {
+  return (
+    <div>Corporations</div>
+  )
+}
+
+export default Corporations;
